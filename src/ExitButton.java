a1b71db7f6a9e0f1ef86f5f1360ed6f2dd61d43c
于家汉 <yujiahan_biancheng@163.com>
2021-10-04 11:47:00 +0800

    猜数游戏(带GUI-Beta版)

--- src/ExitButton.java
+++ src/ExitButton.java
@@ -1,13 +1,17 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ExitButton implements Interface {
+    private int k = new RandomNumber().getRandom();
+
     public void action() {
         panel.add(exit);
 
         exit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"The random-number is " + k);
                 System.exit(0);
             }
         });
